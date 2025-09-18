651d95bb999a3f50473bc41a47d18500e1ce0476
hariharan <hariharan@humworld.com>
2025-09-19 00:30:59 +0530

    Modified the List_S3.py

--- List_S3.py
+++ List_S3.py
@@ -5,7 +5,10 @@ for bucket in s3.buckets.all():
     print(bucket.name)
 
 
-print("\n\n")
-bucket = s3.Bucket('boto3-s3-bucket-upload-test')
+print("\n")
+bucket_name = 'your-bucket-name'
+bucket = s3.Bucket(bucket_name)
+print(bucket.name)
+print("\n")
 for obj in bucket.objects.all():
     print(obj.key)
