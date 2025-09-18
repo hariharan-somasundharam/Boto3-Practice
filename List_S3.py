de9e77a1c4add182b8f03ca4f0fa9687d990dcce
hariharan <hariharan@humworld.com>
2025-09-19 02:27:42 +0530

    Modified the List_S3.py

--- List_S3.py
+++ List_S3.py
@@ -1,18 +1,18 @@
 import boto3
 
-# s3 = boto3.resource('s3')
-# for bucket in s3.buckets.all():
-#     print(bucket.name)
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket.name)
 
 
-# print("\n")
-# bucket_name = 'your-bucket-name'
-# bucket = s3.Bucket(bucket_name)
-# print(bucket.name)
-# print("\n")
-# for obj in bucket.objects.all():
-#     print(obj.key)
+print("\n")
+bucket_name = 'your-bucket-name'
+bucket = s3.Bucket(bucket_name)
+print(bucket.name)
+print("\n")
+for obj in bucket.objects.all():
+    print(obj.key)
 
-S3 = boto3.client('s3')
-bucket_list = S3.list_buckets()
-print(bucket_list)
+# S3 = boto3.client('s3')
+# bucket_list = S3.list_buckets()
+# print(bucket_list)
